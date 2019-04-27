683f8462c29f4cefea871a31ca472a89470b4bfa
Nicholas Altunian <naaltunian@icloud.com>
2019-04-27 15:10:35 -0400

    Fix route bugs

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -6,8 +6,9 @@ import logo from './images/sc.png';
 const Navbar = () => {
     return(
         <div className="navbar">
-            <Link to="/dashboard"><img src={logo} className="logo" alt="basketball" height="50px" /></Link>
+            <Link to="/"><img src={logo} className="logo" alt="logoPicture" height="50px" /></Link>
             <Link className="link" to="/about">About</Link>
+            <Link className="link" to="/dashboard">Dashboard</Link>
             <span className="push">
                 <Link className="link" to="/register">Register</Link>
                 <Link className="link" to="/login">Login</Link>
