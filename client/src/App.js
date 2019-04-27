26aeb07cf4c83ff3b9e02b39150f24bbc1a25d64
Nicholas Altunian <naltunian@carecloud.com>
2019-04-27 09:58:44 -0400

    Set up login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import  Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from './Components/Login/Login';
 
 class App extends React.Component {
   render() {
     return(
       <Router>
         <Navbar />
-
         
       </Router>
     )
