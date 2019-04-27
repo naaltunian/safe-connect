bfb40e362553121479788c591bf7b5829d0a3bd9
Leo Policastro <43428385+leopolicastro@users.noreply.github.com>
2019-04-27 09:04:14 -0400

    Revert "Node backend"

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import  Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  render() {
-    return(
-      <Router>
-        <Navbar />
-
-        
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
