68d0b42810bda81d65d14774a4436101e3de0631
Robert B Garcia <robgarcia552@gmail.com>
2019-04-27 15:24:24 -0400

    add contacts form and links

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from './Components/Login/Login';
 import Register from './Components/Registration/Register';
 import Dashboard from './Components/Dashboard/Dashboard';
+import AddContacts from './Components/AddContacts/AddContacts';
 
 
 class App extends React.Component {
@@ -17,6 +18,7 @@ class App extends React.Component {
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path='/dashboard' component={Dashboard} />
+        <Route path='/add-contacts' component={AddContacts}/>
         
       </Router>
     )
