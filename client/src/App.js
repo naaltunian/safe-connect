5624e8b8cf2417d12eb1a318d976fb40a037b17b
Robert B Garcia <robgarcia552@gmail.com>
2019-04-27 11:11:42 -0400

    remove registration component from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import  Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Registration from './Components/Registration/step-1';
 
 class App extends React.Component {
   render() {
